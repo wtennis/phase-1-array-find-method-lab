4bfd2f92478f3516aceeeb10a8187a3858e17395
Whiting <whitinghtennis@gmail.com>
2021-06-30 10:56:20 -0700

    commit tree 03b020f0fc38ca51b25d73834c2c0abcbed4ca5d

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 
+
+function superbowlWin(array) {
+    function iteratorFunc(array) {
+      return (array.result === "W");
+  }
+    return ((array.find(iteratorFunc)) ? (array.find(iteratorFunc).year) : undefined);
+  }
+  
